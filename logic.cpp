2e417e197cb865f26cb51c58572a5b7178076f96
richard <richard@somovidis.com>
2021-11-08 15:25:11 +0000

    Added Conway's game of life logic

--- logic.cpp
+++ logic.cpp
@@ -8,6 +8,16 @@ MatrixXd initWorld(int worldWidth, int worldHeight)
 {
 	// Create the world as a matrix
 	MatrixXd world = MatrixXd::Zero(worldHeight, worldWidth);
+	// Fill the cells randomly
+	srand (time(NULL));
+	for (int i = 1; i < worldWidth - 1; i++)
+	{
+		for (int j = 1; j < worldHeight; j++)
+		{
+			world(j, i) = rand() % 2;
+		}
+
+	}
 	return world;
 }
 
@@ -16,6 +26,31 @@ MatrixXd step(MatrixXd world)
 	int worldWidth = world.cols();
 	int worldHeight = world.rows();
 	MatrixXd new_world = MatrixXd::Zero(worldHeight, worldWidth);
+	// Update every cell
+	for (int i = 1; i < worldWidth - 1; i++)
+	{
+		for (int j = 1; j < worldHeight - 1; j++)
+		{
+			// Yes it's ugly, yes I need to fix it
+			int sum = 0;
+			sum += world(j - 1, i - 1);
+			sum += world(j - 1, i);
+			sum += world(j - 1, i + 1);
+			sum += world(j, i - 1);
+			sum += world(j, i + 1);
+			sum += world(j + 1, i - 1);
+			sum += world(j + 1, i);
+			sum += world(j + 1, i + 1);
+
+			// Standard Conway's GOL rules
+			if (sum < 2 || sum > 3)
+				new_world(j, i) = 0;
+			else if (sum == 3)
+				new_world(j, i) = 1;
+			else if (sum == 2)
+				new_world(j, i) = world(j, i);
+		}
+	}
 
 	return new_world;
 }
